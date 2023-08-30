20f26898de54bea812e9e17869a8aa411f03e113
James Gino Mugnolo <72045640+JamesMugnolo@users.noreply.github.com>
2023-08-29 20:05:33 -0500

    Updated Connection to DB

--- My-Portfolio-Backend/app/server/config/databaseConfig.ts
+++ My-Portfolio-Backend/app/server/config/databaseConfig.ts
@@ -1,7 +1,12 @@
 const { Client } = require("pg");
 
-const DBurl = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
-const client = new Client(DBurl);
+const client = new Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 
 client.connect((err) => {
   if (err) console.error("connection error", err.stack);
