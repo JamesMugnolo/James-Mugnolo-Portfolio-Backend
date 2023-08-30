057fe29fa590dcf617e30381d5bfd1d1ffdcc291
James Gino Mugnolo <72045640+JamesMugnolo@users.noreply.github.com>
2023-08-29 23:07:19 -0500

    Update Server.js

--- My-Portfolio-Backend/app/server/Server.js
+++ My-Portfolio-Backend/app/server/Server.js
@@ -5,6 +5,12 @@ const apiRouter = require("./routes");
 const app = express();
 app.use("/api", apiRouter);
 app.use(cors({ origin: "https://jamesmugnoloportfolio.netlify.app" }));
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://jamesmugnoloportfolio.netlify.app"
+  );
+});
 app.use(express.json());
 
 app.listen(3000);
