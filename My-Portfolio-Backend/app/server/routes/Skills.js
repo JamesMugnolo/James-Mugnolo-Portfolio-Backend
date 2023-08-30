c55d732a3be6c0b4fc39b402e125f4ab0e3267ae
James Gino Mugnolo <72045640+JamesMugnolo@users.noreply.github.com>
2023-08-29 23:34:53 -0500

    Update Skills.js

--- My-Portfolio-Backend/app/server/routes/Skills.js
+++ My-Portfolio-Backend/app/server/routes/Skills.js
@@ -1,7 +1,8 @@
 const express = require("express");
 let client = require("../config/databaseConfig.ts");
 let router = express.Router();
-
+let cors = require("cors");
+router.use(cors({ origin: "https://jamesmugnoloportfolio.netlify.app" }));
 router.get("/:type", async function (req, res) {
   try {
     const type = req.params.type;
